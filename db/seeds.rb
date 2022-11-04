57b4910b0737b718c5647982cfd24f60452f121a
Sara Raley <sara.m.raley@gmail.com>
2022-11-04 11:59:15 -0600

    init and adds data to seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Song.create(title: "Colorado Girl", album: "Our Mother the Mountain", artist: "Townes Van Zandt", year: 1969)
+Song.create(title: "Forty Miles From Denver", album: "Elevation", artist: "Yonder Mountain String Band", year: 1999)
+Song.create(title: "You Wild Colorador", album: "Orange Blossom Special", artist: "Johnny Cash", year: 1965)
